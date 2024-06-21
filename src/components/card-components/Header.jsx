9b3204be8599a46b0ce178daffade31f0312d932
jitchu <9021012@student.zadkine.nl>
2024-06-21 23:52:42 +0200

    right file path fix

--- src/components/card-components/Header.jsx
+++ src/components/card-components/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
     return ( 
     <div className="w-full card-bg flex flex-col items-center p-2 mb-3">
         <div className="userInfo flex flex-col items-center p-5 ">
-            <img className="rounded-full h-28" src="../../public/images/pfp.gif" alt="" />
+            <img className="rounded-full h-28" src="/public/images/pfp.gif" alt="" />
             <IconContext.Provider value={{ className: 'wobble' }}>
             <div className="flex flex-row justify-center mt-2 w-full">
             <h2 className=" font-bold text-xl p-2">jitchu </h2>

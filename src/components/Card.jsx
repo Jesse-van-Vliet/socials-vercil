0cedf5203e65a97b079ef23f07ae115844027018
jitchu <9021012@student.zadkine.nl>
2024-06-22 14:42:28 +0200

    paralex overflow issue fixed

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,3 @@
-
 import AudioPlayer from "./card-components/AudioPlayer";
 import Header from "./card-components/Header";
 import React from 'react';
@@ -7,9 +6,9 @@ import Tilt from 'react-parallax-tilt';
 const Card = () => {
     return (
 
-        <div className=" w-5/12 flex flex-col  text-white color p-4 m-5 card-r">
+        <div className=" w-5/12 flex flex-col text-white color p-4 m-5 card-r">
 
-            <Tilt tiltMaxAngleX={10} tiltMaxAngleY={10} tiltReverse={true}>
+            <Tilt tiltMaxAngleX={8} tiltMaxAngleY={8} tiltReverse={true}>
                 <Header />
                 <AudioPlayer />
             </Tilt>
